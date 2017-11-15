7ae127444f01b0f0644afaa79a299122e8abedbe
adayc <adayc@hotmail.com>
2017-11-15 03:50:27 -0500

    Estructura HTML finalizada

--- js/app.js
+++ js/app.js
@@ -1,26 +1,12 @@
+var mostrarOcultar =function(){
+  alert('hola chicas');
+};
 
-
-// Declaramos las funciones que se emplearan al cargar la página
-// window.onload = function() {
-// Declaración de las variables  
-var submenu = document.getElementsByClassName('submenu');
-for (i = 0;i < submenu.length;i++) {
-  submenu[i].addEventListener('click', showMenu);
-}
-// };
-
-
-// this hace referencia al elemento al cual le estas dando click en ese momento
-function showMenu() {
-  // Buscará al primer elemento que tenga la clase itemList dentro del elemento al que le dimos click
-  var listMenu = this.getElementsByClassName('itemList')[0];
-  // En el caso que tenga la clase hide la removerá y agregara show
-  if (listMenu.classList.contains('hide')) {
-    listMenu.classList.remove('hide');
-    listMenu.classList.add('show');
-  } else {
-  // En el caso que tenga la clase show la removerá y agregara hide
-    listMenu.classList.add('hide');
-    listMenu.classList.remove('show');
+var cargarPagina=function(){
+  var elementosTab=document.getElementsByClassName("tab");
+  for(var i=0; i<elementosTab.length;i++){
+    elementosTab[i].addEventListener("click",mostrarOcultar);
   }
 }
+
+cargarPagina();
